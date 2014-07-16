da14aedeaccdbbceff397322c051a724c73e4f6e
RedRingRico <teh_programerer@interactivelaboratories.com>
2014-07-16 14:47:18 +0100

    Added a prototype for the project information template
    |WEB|
    [projects/index.php]
    * Added a mock-up project information table

--- projects/index.php
+++ projects/index.php
@@ -10,7 +10,63 @@
 			<?php SiteHeader( ); ?>
 
 			<div id="body">
-				This is where the projects will be
+				<div style="font-size:20px">
+					Category: <a href="" style="font-size:20px">Game</a> | <a href="" style="font-size:20px">Tech Demo</a>
+				</div>
+				<table style="width:750px">
+					<tr style="background:#600000">
+						<!-- Main category / sub-category -->
+						<td colspan="2" style="font-size:20px">Game // Puzzle Action Platformer Racing Murder Simulator</td>
+					</tr>
+					<tr style="background:#730000">
+						<!-- Title -->
+						<td colspan="2">Mock Game</td>
+					</tr>
+					<tr style="background:#600000">
+						<!-- A small thumbnail of a screenshot -->
+						<td><img src="mock/thumbnail.png" width=128 height=128/></td>
+						<!-- Description -->
+						<td style="vertical-align:text-top;">
+							This is a game about throwing balls and hitting other people!
+						</td>
+					</tr>
+					<tr style="background:#730000">
+						<td style="vertical-align:text-top">
+							<a href="">Screenshots</a>
+						</td>
+						<td>
+							Source Code
+							<br/>
+							<a href="">Dreamcast</a>
+							<br/>
+							<a href="">Linux</a>
+							<br/>
+							<a href="">Windows</a>
+						</td>
+					</br>
+					<tr style="background:#600000">
+						<td style="vertical-align:text-top">
+							<a href="">Videos</a>
+						</td>
+						<td>
+							Download
+							<br/>
+							<a href="">Dreamcast</a>
+							<br/>
+							<a href="">Linux</a>
+							<br/>
+							<a href="">Windows</a>
+						</td>
+					</tr>
+					<tr style="background:#730000">
+						<!-- Tags -->
+						<td colspan="2">Tags: <a href="">Fun</a>, <a href="">Game</a>, <a href="">Questionable Physics</a></td>
+					</br>
+					<tr style="background:#600000">
+						<!-- Platforms -->
+						<td colspan="2">Platforms: <a href="">Xbox</a>, <a href="">Dreamcast</a>, <a href="">Linux</a>, <a href="">Windows</a></td>
+					</tr>
+				</table>
 			</div>
 
 			<?php SiteFooter( ); ?>
