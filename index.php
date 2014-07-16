02683a840d50dea2498b74de317a748e28bb13df
RedRingRico <teh_programerer@interactivelaboratories.com>
2014-07-16 10:59:26 +0100

    Added a CSS file to improve the look of the site
    |WEB|
    [/index.php]
    * Changed the background colour to a deeper red
    * Using the VT323 font from Google Fonts
    * The CSS file is used to format the page
    [/common/default.css]
    * The contents of the site are limited to 770px wide to accomodate most web
      browser windows
    * The header id provides enough spacing to allow the header to not interfere
      with the page's contents
    * The footer id is used to force the footer section to the floor of the page

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <html>
 	<head>
 		<title>Red Ring Rico's Game Programming Web Zone</title>
+		<link href='http://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
+		<link href="common/default.css" rel="stylesheet" type="text/css">
 	</head>
-	<body style="background:#B20000; color:#FFFF00">
-		<p>
-			Welcome to Red Ring Rico's Game Programming Web Zone!
-		</p>
-		<p>
-			This is a website for various game technology demos, games, libraries, engines, and tools.
-		</p>
-		<p>
-			Copyright <?php echo date( 'Y' )?> Rico
-		</p>
+	<body>
+		<div id="content">
+			<div id="header">
+				Welcome to Red Ring Rico's Game Programming Web Zone!
+			</div>
+
+			<div id="body">
+				This is a website for various game technology demos, games, libraries, engines, and tools.
+			</div>
+
+			<div id="footer">
+				Copyright <?php echo date( 'Y' )?> Rico
+			</div>
 	</body>
 </html>
