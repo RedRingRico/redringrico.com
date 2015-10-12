4fe77979e93688daba7a48f39fc3790083e3e5f0
Rico <teh_programerer@interactivelaboratories.com>
2015-10-12 22:36:50 +0100

    Added an actual project to the Projects page

--- projects/index.php
+++ projects/index.php
@@ -1,8 +1,8 @@
 <?php require_once( '../common/site.php' ) ?>
 <html>
 	<head>
-		<title>Red Ring Rico's Game Programming Web Zone</title>
-		<link href='http://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
+		<title>Red Ring Rico's Game Programming Web Zone | Projects</title>
+		<link href='https://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
 		<link href="../common/default.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
@@ -10,61 +10,66 @@
 			<?php SiteHeader( ); ?>
 
 			<div id="body">
-				<div style="font-size:20px">
+				<!--<div style="font-size:20px">
 					Category: <a href="" style="font-size:20px">Game</a> | <a href="" style="font-size:20px">Tech Demo</a> | <a href="" style="font-size:20px">Tool</a> | <a href="" style="font-size:20px">Engine</a>
-				</div>
+				</div>-->
 				<table style="width:750px">
 					<tr style="background:#600000">
 						<!-- Main category / sub-category -->
-						<td colspan="2" style="font-size:20px">Game // Puzzle Action Platformer Racing Murder Simulator</td>
+						<td colspan="2" style="font-size:20px">Tool // SDK</td>
 					</tr>
 					<tr style="background:#730000">
 						<!-- Title -->
-						<td colspan="2">Mock Game</td>
+						<td colspan="2">SEGA Saturn SDK</td>
 					</tr>
 					<tr style="background:#600000">
-						<!-- A small thumbnail of a screenshot -->
-						<td><img src="mock/thumbnail.png" width=128 height=128/></td>
+						<!-- A small thumbnail of a screenshot/logo -->
+						<td><img src="sssdk/thumbnail.png" width=128 height=128/></td>
 						<!-- Description -->
 						<td style="vertical-align:text-top;">
-							This is a game about throwing balls and hitting other people!
+							The SEGA Saturn SDK (SSSDK) is a Free and Open Source software development kit for developing games on the SEGA Saturn.
 						</td>
 					</tr>
 					<tr style="background:#730000">
 						<td style="vertical-align:text-top">
-							<a href="">Screenshots</a>
+							<!--<a href="">Screenshots</a>-->
 						</td>
 						<td>
-							Source Code
+							Source Code Repositories | <a href="https://ci.opengamedevelopers.org/view/SEGA Saturn SDK">Build Server</a>
 							<br/>
-							<a href="">Dreamcast</a>
+							<a href="https://github.com/SaturnSDK/Saturn-SDK-GCC-SH2">GCC for SH-2</a>
 							<br/>
-							<a href="">Linux</a>
+							<a href="https://github.com/SaturnSDK/Saturn-SDK-GNU-Make">GNU Make</a>
 							<br/>
-							<a href="">Windows</a>
+							<a href="https://github.com/SaturnSDK/Saturn-SDK-IDE">Code::Blocks IDE for SSSDK</a>
+							<br/>
+							<a href="https://github.com/SaturnSDK/Saturn-SDK-MSYS2">MSYS 2 for SSSDK</a>
+							<br/>
+							<a href="https://github.com/SaturnSDK/Saturn-SDK-Installer">Installer</a>
 						</td>
 					</br>
 					<tr style="background:#600000">
 						<td style="vertical-align:text-top">
-							<a href="">Videos</a>
+							<a href="https://saturnsdk.github.io/samples.html">Videos</a> (External site)
 						</td>
 						<td>
 							Download
 							<br/>
-							<a href="">Dreamcast</a>
+							<a href="ftp://ftp.opengamedevelopers.org/saturn-sdk/installer/SEGA-Saturn-SDK_i686-pc-linux-gnu_alpha_latest.run">GNU/Linux 32-bit</a>
 							<br/>
-							<a href="">Linux</a>
+							<a href="ftp://ftp.opengamedevelopers.org/saturn-sdk/installer/SEGA-Saturn-SDK_x86_64-pc-linux-gnu_alpha_latest.run">GNU/Linux 64-bit</a>
 							<br/>
-							<a href="">Windows</a>
+							<a href="ftp://ftp.opengamedevelopers.org/saturn-sdk/installer/SEGA-Saturn-SDK_i686-w64-mingw32_alpha_latest.exe">Windows 32-bit</a>
+							<br/>
+							<a href="ftp://ftp.opengamedevelopers.org/saturn-sdk/installer/SEGA-Saturn-SDK_x86_64-w64-mingw32_alpha_latest.exe">Windows 64-bit</a>
 						</td>
 					</tr>
-					<tr style="background:#730000">
-						<!-- Tags -->
+					<!--<tr style="background:#730000">
 						<td colspan="2">Tags: <a href="">Fun</a>, <a href="">Game</a>, <a href="">Questionable Physics</a></td>
-					</br>
+					</br>-->
 					<tr style="background:#600000">
 						<!-- Platforms -->
-						<td colspan="2">Platforms: <a href="">Xbox</a>, <a href="">Dreamcast</a>, <a href="">Linux</a>, <a href="">Windows</a></td>
+						<td colspan="2">Platforms: Linux, Windows</td>
 					</tr>
 				</table>
 			</div>
